bf0e13e660f6658dfd36bfece1c7363b734ac499
Feli <feliciajoshlyn@gmail.com>
2025-12-09 09:15:28 +0700

    Fix category bar component name in news index view
    Updated the Blade component from <x-category-bar> to <x-category_bar> to match the correct component naming convention.

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -38,7 +38,7 @@
 
         {{-- category picker --}}
         <div class="mb-8 flex justify-center">
-            <x-category-bar :categories="$categories" :selected="request('category')" :summary="$summary" />
+            <x-category_bar :categories="$categories" :selected="request('category')" :summary="$summary" />
         </div>
 
         @if ($search || request('category'))
